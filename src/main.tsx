9800d5668e88e7523c24dd869ef34d59c43b3c60
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-01-03 14:15:31 +0530

    build(deps): added sass

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 // Import Global Styles
-import './style.css'
+import './style.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
